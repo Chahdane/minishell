7402e3c1e691c29b6dca723322651ce46d409d93
Abderrahmane Chahdane <a.chahdane@gmail.com>
2022-08-10 20:20:29 +0100

    makefile added

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: owahdani <marvin@42.fr>                    +#+  +:+       +#+         #
+#    By: achahdan <achahdan@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/07/19 20:16:21 by owahdani          #+#    #+#              #
-#    Updated: 2022/08/07 00:01:49 by owahdani         ###   ########.fr        #
+#    Updated: 2022/08/10 20:18:58 by achahdan         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -32,7 +32,10 @@ SRC = error_handlers.c \
 	  environment.c \
 	  expansion.c \
 	  expansion_2.c \
-	  utils.c
+	  utils.c \
+	  b_env.c \
+	  b_export.c \
+	  b_export2.c
 
 SRC := $(addprefix $(SRC_DIR),$(SRC))
 OBJ = $(patsubst $(SRC_DIR)%.c,%.o,$(SRC))
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: owahdani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: achahdan <achahdan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 20:18:47 by owahdani          #+#    #+#             */
-/*   Updated: 2022/08/07 14:39:52 by owahdani         ###   ########.fr       */
+/*   Updated: 2022/08/10 19:26:48 by achahdan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+
 
 int	main(int ac, char **av, char **envp)
 {
@@ -19,15 +21,21 @@ int	main(int ac, char **av, char **envp)
 	(void) ac;
 	(void) av;
 	init_env_lst(envp);
+
+	//return 0;
 	line = readline(PROMPT);
 	while (line)
 	{
 		ft_add_history(line);
 		/* if !parse_line, execute*/
 		parse_line(line);
+		//export(g_data.env_lst, g_data.cmds->args);
+		//env(g_data.env_lst);
+		print_in();
 		free(line);
 		clear_cmds_lst(g_data.cmds);
 		line = readline(PROMPT);
 	}
+	
 	return (0);
 }
