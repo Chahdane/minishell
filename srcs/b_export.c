736a6036efedeb454c535797f8579f8c16ed659f
Abderrahmane Chahdane <a.chahdane@gmail.com>
2022-08-10 20:45:07 +0100

    b_export working

--- srcs/b_export.c
+++ srcs/b_export.c
@@ -6,7 +6,7 @@
 /*   By: achahdan <achahdan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 21:26:49 by achahdan          #+#    #+#             */
-/*   Updated: 2022/08/10 20:24:48 by achahdan         ###   ########.fr       */
+/*   Updated: 2022/08/10 20:44:35 by achahdan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,7 @@ int	check_naming(char *str)
 	i = 1;
 	if (!ft_isalpha(str[0]) && str[0] != '_')
 	{
-		ft_perror("minishell", "export: `': not a valid identifier", 0);
+		ft_perror("minishell export: ", "not a valid identifier", 0);
 		return (-1);
 	}
 	while (str[i])
@@ -94,52 +94,6 @@ int	check_naming(char *str)
 	}
 	return (1);
 }
-
-t_env	*get_min(int index)
-{
-	t_env	*env;
-	t_env	*min;
-	int		i;
-
-	i = 0;
-	env = g_data.env_lst;
-	//min = env;
-	while (env->next)
-	{
-		if (ft_strcmp(env->var,env->next->var) > 0 && env->next->index > index)
-			min = env->next;
-		env = env->next;
-	}
-	min->index = index;
-	return min;
-}
-
-void	print_in_order()
-{
-	t_env *env;
-	t_env *temp;
-	env = g_data.env_lst;
-	int i = 1;
-	int len = 0;
-	temp = env;
-	t_env *print;
-
-	while(temp)
-	{
-		temp->index = 0;
-		//printf("%d-",temp->index);
-		temp = temp->next;
-		len++;
-	}
-	while (i <= len)
-	{
-		print = get_min(i);
-		printf("%s\n",print->var);
-		i++;
-	}
-
-}
-
 
 void	export(t_env *env, char **args)
 {
