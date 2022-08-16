67f9e261b553332f86f1e44a2bb4ddc609a25c61
Abderrahmane Chahdane <a.chahdane@gmail.com>
2022-08-16 22:57:48 +0100

    check path added

--- srcs/accessibility.c
+++ srcs/accessibility.c
@@ -3,13 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   accessibility.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: owahdani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: achahdan <achahdan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 17:34:49 by owahdani          #+#    #+#             */
-/*   Updated: 2022/08/16 21:33:23 by owahdani         ###   ########.fr       */
+/*   Updated: 2022/08/16 22:57:14 by achahdan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <minishell.h>
+#include <minishell.h>
 
-char	*
+char	*check_path(char *cmd, t_env *env)
+{
+	char	**paths;
+	char	*temp;
+	char	*path_env;
+
+	while (env)
+	{
+		if (!ft_strcmp(env->var, "PATH"))
+			break ;
+		env = env->next;
+	}
+	if (env == NULL || env->value == NULL)
+		return (NULL);
+	paths = ft_split(env->value, ':');
+	while (*paths)
+	{
+		temp = ft_strjoin(*paths, '/');
+		path_env = ft_strjoin(path_env, cmd);
+		free(temp);
+		if (access(path_env, F_OK) == 0)
+			return (path_env);
+		paths++;
+		free(path_env);
+	}
+	return (NULL);
+}
